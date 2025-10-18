47faa7007eeaaad1f07f9d54df2c1f6c2849f2e6
kunal <kunalghosh2321@gmail.com>
2025-10-18 19:24:06 +0530

    working on deploy jetton token steup

--- scripts/deployJettonToken.ts
+++ scripts/deployJettonToken.ts
@@ -1,8 +1,22 @@
 import { toNano } from '@ton/core';
 import { JettonToken } from '../build/JettonToken/JettonToken_JettonToken';
 import { NetworkProvider } from '@ton/blueprint';
+import { buildOnchainMetadata } from '../utils/jetton-helpers';
 
 export async function run(provider: NetworkProvider) {
+     const jettonParams = {
+        name: "Mushafa Token",
+        description: "This is a test token for testing purposes only.",
+        symbol: "MUSH",
+        image: "https://azure-elegant-gibbon-704.mypinata.cloud/ipfs/bafybeibcyncpqdatxt2nh6bmkjkuqzclqotshfliggvvk6levro66y32du",
+    };
+
+    const totalSupply = 1000;
+    const owner = process.env.JETTON_TOKEN_OWNER_ADDRESS;
+    
+
+    // Create content Cell
+    let content = buildOnchainMetadata(jettonParams);
     const jettonToken = provider.open(await JettonToken.fromInit());
 
     await jettonToken.send(
@@ -14,6 +28,7 @@ export async function run(provider: NetworkProvider) {
     );
 
     await provider.waitForDeploy(jettonToken.address);
+    console.log('JettonToken deployed to', jettonToken.address);
 
     // run methods on `jettonToken`
 }
