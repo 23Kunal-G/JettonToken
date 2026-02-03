6dc8a6356fa648c841a3ef8672ed15617c16db23
Kunal Ghosh <kunalghosh2321@gmail.com>
2026-02-03 23:28:19 +0530

    update maxSupply part

--- scripts/deployJettonToken.ts
+++ scripts/deployJettonToken.ts
@@ -1,4 +1,4 @@
-import { toNano } from '@ton/core';
+import { Address, toNano } from '@ton/core';
 import { JettonToken } from '../build/JettonToken/JettonToken_JettonToken';
 import { NetworkProvider } from '@ton/blueprint';
 import { buildOnchainMetadata } from '../utils/jetton-helpers';
@@ -11,24 +11,28 @@ export async function run(provider: NetworkProvider) {
         image: "https://azure-elegant-gibbon-704.mypinata.cloud/ipfs/bafybeibcyncpqdatxt2nh6bmkjkuqzclqotshfliggvvk6levro66y32du",
     };
 
-    const totalSupply = 1000;
-    const owner = process.env.JETTON_TOKEN_OWNER_ADDRESS;
+    const totalSupply = 1000_000_000_000n;
+    const owner = Address.parse(process.env.JETTON_TOKEN_OWNER_ADDRESS as string);
     
 
     // Create content Cell
     let content = buildOnchainMetadata(jettonParams);
-    const jettonToken = provider.open(await JettonToken.fromInit());
+    const jettonToken = provider.open(await JettonToken.fromInit(owner, content, totalSupply));
 
     await jettonToken.send(
         provider.sender(),
         {
             value: toNano('0.05'),
         },
-        null,
+        {
+            $$type: 'Mint',
+            amount: 100_000_000_000n, // 100 tokens with 9 decimals
+            receiver: owner,
+        },
     );
 
     await provider.waitForDeploy(jettonToken.address);
-    console.log('JettonToken deployed to', jettonToken.address);
+    console.log('JettonToken deployed to', jettonToken.address.toString());
 
     // run methods on `jettonToken`
 }
